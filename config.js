387d68279c86b97923840828e53ec482073e4c97
vishalsahilai <vishalsahilofficial@gmail.com>
2026-08-26 10:02:22 -0700

    remove

--- config.js
+++ config.js
@@ -20,7 +20,7 @@ var AP_CONFIG = {
     // See README section "One-time Google Cloud setup".
     // Authorised redirect URI to register:
     //   https://cpmepjhamnhjgojlkcmnopphmpcogbin.chromiumapp.org/
-    clientId: '858882040824-gltnfkdej1ll81gr5b32hfdjqh9appe2.apps.googleusercontent.com',
+    clientId: '',
 
     // Least-privilege Drive scope: the extension can only see and touch files
     // that the extension itself created. It can never read your other files.
